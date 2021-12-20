f0d39e86cdecbea78edabe5c878dcdfbcff6b084
huulamnguyen <liamnguyen.swe@gmail.com>
2021-12-19 20:09:06 -0500

    commit tree 37d3e2074abe3cf910686c7ff5edee87f13796d8

--- index.js
+++ index.js
@@ -1,9 +1,18 @@
-function addingEventListener() {
-    const input = document.getElementById('input');
-    function clickAlert() {
-        alert('I was clicked!');
-};
-    input.addEventListener('click', clickAlert);
-};
+// function addingEventListener() {
+//     const input = document.getElementById('input');
+//     function clickAlert() {
+//         alert('I was clicked!');
+// };
+//     input.addEventListener('click', clickAlert);
+// };
 
+// addingEventListener();
+
+function addingEventListener() {
+    const input = document.querySelector('#input');
+    function alertMe() {
+        return alert("I was clicked!")
+    }
+    input.addEventListener('click', alertMe)
+}
 addingEventListener();
